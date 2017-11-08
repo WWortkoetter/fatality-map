9e66dd1d22339c1d54106086e70e2f0a23046cb5
WWortkoetter <32912778+WWortkoetter@users.noreply.github.com>
2017-11-07 21:59:01 -0500

    can now turn heatmap on, but not off

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ var myFunctionHolder = {};
 //declaring function 1
 myFunctionHolder.addPopups = function (feature, layer) {
     if (feature.properties /*&& feature.properties.age*/) {
-    layer.bindPopup(
-        "<dl><dt>Location: </dt>" + feature.properties['trafid1']
-        + "<dt>Number Killed: </dt>" + feature.properties['numfatal']
-        + "<dt>Age: </dt>" + feature.properties['age']
-        + "<dt>Coordinates </dt>" + feature.geometry['coordinates']
-        + "<dt>Year: </dt>" + feature.properties['caseyear']
-    );
+        layer.bindPopup(
+            "<dl><dt>Location: </dt>" + feature.properties['trafid1']
+            + "<dt>Number Killed: </dt>" + feature.properties['numfatal']
+            + "<dt>Age: </dt>" + feature.properties['age']
+            + "<dt>Coordinates </dt>" + feature.geometry['coordinates']
+            + "<dt>Year: </dt>" + feature.properties['caseyear']
+        );
     }
 }
-    
+
 //declaring function 2
 myFunctionHolder.pointToCircle = function (feature, latlng) {
     var geojsonMarkerOptions = {
@@ -37,9 +37,9 @@ window.onload = function () {
         maxZoom: 18,
         attribution: "&copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
     }).addTo(mapObject);
-    
+
     // fatalities is the variable name we defined in point data file. 
-    
+
     var fatalsLayerGroup = L.geoJSON(fatalities, {
         onEachFeature: myFunctionHolder.addPopups,
         pointToLayer: myFunctionHolder.pointToCircle
@@ -47,14 +47,14 @@ window.onload = function () {
 
     mapObject.addLayer(fatalsLayerGroup);
     mapObject.fitBounds(fatalsLayerGroup.getBounds());
-        
+
     var cfg = {
         // radius should be small ONLY if scaleRadius is true (or small radius is intended)
         // if scaleRadius is false it will be the constant radius used in pixels
         "radius": 0.015,
-        "maxOpacity": .8, 
+        "maxOpacity": .8,
         // scales the radius based on map zoom
-        "scaleRadius": true, 
+        "scaleRadius": true,
         // if set to false the heatmap uses the global maximum for colorization
         // if activated: uses the data maximum within the current map boundaries 
         //   (there will always be a red spot with useLocalExtremas true)
@@ -66,13 +66,20 @@ window.onload = function () {
         // which field name in your data represents the data value - default "value"
         valueField: 'value'
     };
-    
+
     var heatmapLayer = new HeatmapOverlay(cfg);
     var heatmapLayerGroup = L.geoJSON().addLayer(heatmapLayer);
 
-    var button = document.getElementById('heatmapToggle');
- 
-    // if (document.getElementById("heatmapToggle").getAttribute("state") === 0){
+    var button = document.getElementById("heatmapToggle");
+
+    button.onclick = function() {
+        heatmapLayer.setData(fatalitiesHeatmapData);
+        heatmapLayerGroup = L.geoJSON().addLayer(heatmapLayer);
+        mapObject.addLayer(heatmapLayer);
+    }
+
+
+    // if (document.getElementById("heatmapToggle").getAttribute("state") == 0) {
     //     print()
     //     mapObject.removeLayer(heatmapLayerGroup);
     // }
