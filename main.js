54e3043ef771dd6abba1ba5cf2e75826a31fcbc7
WWortkoetter <32912778+WWortkoetter@users.noreply.github.com>
2017-11-08 20:40:29 -0500

    added a title (also added satellite toggle)

--- main.js
+++ main.js
@@ -1,58 +1,146 @@
 "use strict";
 
-var myFunctionHolder = {};
-
-//declaring function 1
-myFunctionHolder.addPopups = function (feature, layer) {
-    var survived = '';
-    if (feature.properties['dthday'] == -1 || feature.properties['dthday'] == 88 || feature.properties['dthday'] == 99){
-        survived = 'Lived';
-    } 
-    else {
-        survived = 'Deceased';
-    }
-
-    if (feature.properties) {
-        layer.bindPopup(
-            "<dl><dt>Location: </dt>" + feature.properties['trafid1']
-            + "<dt>Number Killed: </dt>" + feature.properties['numfatal']
-            + "<dt>Age: </dt>" + feature.properties['age']
-            + "<dt>Year: </dt>" + feature.properties['caseyear']
-            + "<dt>Status: </dt>" + survived
-        );
-    }
-}
-
-//declaring function 2
-myFunctionHolder.pointToCircle = function (feature, latlng) {
-    var geojsonMarkerOptions = {
-        radius: 8,
-        fillColor: "greenyellow",
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: .8
-    };
-    var circleMarker = L.circleMarker(latlng, geojsonMarkerOptions);
-    return circleMarker;
-}
-
-
-
 //execute only when window is fully loaded
 window.onload = function () {
+    var myFunctionHolder = {};
+    
+    //declaring popups function
+    myFunctionHolder.addPopups = function (feature, layer) {
+        var survived = '';
+        if (feature.properties['dthday'] == -1 || feature.properties['dthday'] == 88 || feature.properties['dthday'] == 99){
+            survived = 'Lived';
+        } 
+        else {
+            survived = 'Deceased';
+        }
+    
+        var years = '';
+        if (feature.properties['age'] == 999){
+            years = 'Unknown';
+        }
+        else {
+            years = feature.properties['age'];
+        }
+    
+        var gender = '';
+        switch(feature.properties.sex){
+            case 1:
+                gender = 'Male';
+                break;
+            case 2:
+                gender = 'Female';
+                break;
+            default:
+                gender = 'Unknown';
+        }
+
+        var race = '';
+        switch(feature.properties.race){
+            case -1:
+                race = 'N/A';
+                break;
+            case 0:
+                race = 'N/A';
+                break;
+            case 1:
+                if (feature.properties.hispanic >= 1 && feature.properties.hispanic <= 6){
+                    race = 'Hispanic';
+                }
+                else {
+                    race = 'White';
+                }
+                break;
+            case 2:
+                race = 'Black';
+                break;
+            case 18:
+                race = 'Asian Indian';
+                break;
+            case 19:
+                race = 'Other Indian';
+                break;
+            case 68:
+                race = 'Other Asian';
+                break;
+            case 98:
+                race = 'Other';
+                break;
+            case 99:
+                race = 'Unknown';
+                break;
+        }
+    
+        if (feature.properties) {
+            layer.bindPopup(
+                "<dl><dt>Date of accident: </dt>" + feature.properties.accmon + " / " + feature.properties.accday + " / " + feature.properties.caseyear
+                + "<dt>Location: </dt>" + feature.properties.trafid1
+                + "<dt>Number Killed: </dt>" + feature.properties.numfatal
+                + "<dt>Age: </dt>" + years
+                + "<dt>Sex: </dt>" + gender
+                + "<dt>Race: </dt>" + race
+                + "<dt>Status: </dt>" + survived
+            );
+            //document.getElementById("info_loc").innerHTML = "Location: " + feature.properties.trafid1;
+        }
+    }
+    
+    //declaring point function
+    myFunctionHolder.pointToCircle = function (feature, latlng) {
+        var geojsonMarkerOptions = {
+            radius: 8,
+            fillColor: "greenyellow",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: .8
+        };
+        var circleMarker = L.circleMarker(latlng, geojsonMarkerOptions);
+        return circleMarker;
+    }
+
     var mapObject = L.map('mapDivId').setView([39.961, -82.998], 11);
 
     var baseMap = L.tileLayer('https://api.mapbox.com/styles/v1/sinba/ciperkjzk001jb6mdcb41o922/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2luYmEiLCJhIjoiY2loMWF6czQxMHdwcnZvbTNvMjVhaWV0MyJ9.zu-djzdfyr3C_Uj2F7noqg', {
         maxZoom: 18,
-        attribution: "&copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+        attribution: "&copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> &copy;"
     }).addTo(mapObject);
+
+    var satToggle = document.getElementById("satToggle");
+    satToggle.onclick = function() {
+        if(!document.getElementById("unchecked3").checked){ // set map to satellite when toggle is on
+            if (mapObject.hasLayer(baseMap)){
+                mapObject.removeLayer(baseMap);
+            }
+            // sat map
+            baseMap = L.tileLayer('https://api.mapbox.com/styles/v1/wwortkoetter/cj9rq59ip1bv72smp6cqi0ymq/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid3dvcnRrb2V0dGVyIiwiYSI6ImNqNnpnbDJkbDAwNWsycm15ZzI2dW1rc2cifQ.z6g-MJ7zdh699j4x_4U80Q', {
+                maxZoom: 18,
+                attribution: "&copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> &copy;"
+            }).addTo(mapObject);
+            document.getElementById("unchecked3").checked = true;
+            
+        }
+        else { // set map to dark theme when toggle is off
+            if (mapObject.hasLayer(baseMap)){
+                mapObject.removeLayer(baseMap);
+            }            
+            // dark map
+            baseMap = L.tileLayer('https://api.mapbox.com/styles/v1/sinba/ciperkjzk001jb6mdcb41o922/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2luYmEiLCJhIjoiY2loMWF6czQxMHdwcnZvbTNvMjVhaWV0MyJ9.zu-djzdfyr3C_Uj2F7noqg', {
+                maxZoom: 18,
+                attribution: "&copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> &copy;"
+            }).addTo(mapObject);
+            document.getElementById("unchecked3").checked = false;
+        }
+    }
 
     // fatality points
     var fatalsLayerGroup = L.geoJSON(fatalities, {
         onEachFeature: myFunctionHolder.addPopups,
         pointToLayer: myFunctionHolder.pointToCircle
     });
+
+    fatalsLayerGroup.onclick = function(){
+        document.getElementById("info_loc").innerHTML = "Location: " + feature.properties['trafid1'];
+    }
 
     mapObject.addLayer(fatalsLayerGroup);
     mapObject.fitBounds(fatalsLayerGroup.getBounds());
